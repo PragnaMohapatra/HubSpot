b5ce828c0fcffd6b270e4686915b13d0614c485a
Pragna Mohapatra <pragnamohapatra@gmail.com>
2025-05-08 01:08:59 -0700

    logging

--- app/dowloader/json_downloader.py
+++ app/dowloader/json_downloader.py
@@ -2,6 +2,7 @@ from typing import List, Optional
 import requests
 import sys
 import os
+import logging
 from app.dowloader.interface import IDownloader
 from app.parser.model import parse_product
 
@@ -10,8 +11,12 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')
 # Add the parent directory to sys.path
 sys.path.append(parent_dir)
 
-
 class JsonDownloader(IDownloader):
+    def __init__(self):
+        super().__init__()
+        self.logger = logging.getLogger(self.__class__.__name__)
+        logging.basicConfig(level=logging.INFO)
+
     def download(self, url: str, destination: str) -> Optional[List[dict]]:
         try:
             # Send a GET request to the URL
@@ -21,17 +26,16 @@ class JsonDownloader(IDownloader):
                 products_raw = json_data.get("products", [])
                 products = [parse_product(p) for p in products_raw]
                 for p in products:
-                    print(p)
+                    self.logger.info(p)
             else:
                 response.raise_for_status()  # Raise an error for bad responses
-            
 
             # Write the content to the destination file
             with open(destination, 'w') as file:
                 file.write(response.text)
 
-            print(f"Downloaded JSON data from {url} to {destination}")
+            self.logger.info(f"Downloaded JSON data from {url} to {destination}")
             return products
         except requests.exceptions.RequestException as e:
-            print(f"Failed to download JSON data from {url}: {e}")
-            return None 
+            self.logger.error(f"Failed to download JSON data from {url}: {e}")
+            return None
