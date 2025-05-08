354c77b26860ab7525939f6223de14e1c7583dbf
Pragna Mohapatra <pragnamohapatra@gmail.com>
2025-05-08 01:07:25 -0700

    logging

--- app/dowloader/interface.py
+++ app/dowloader/interface.py
@@ -1,10 +1,15 @@
 from abc import ABC, abstractmethod
 from typing import List, Optional
+import logging
 
 class IDownloader(ABC):
     """
     Abstract base class for a downloader.
     """
+
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        logging.basicConfig(level=logging.INFO)
 
     @abstractmethod
     def download(self, url: str, destination: str) -> Optional[List[dict]]:
@@ -14,5 +19,6 @@ class IDownloader(ABC):
         :param url: The URL of the file to download.
         :param destination: The local path where the file should be saved.
         """
+        self.logger.info(f"Downloading from {url} to {destination}")
         pass
 
